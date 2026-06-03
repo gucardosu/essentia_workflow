e163c6619a8944294602022eada7a32f73980eb1
gucardosu <gustavocardosodot@gmail.com>
2026-06-03 17:47:18 -0300

    feat: mais arquivo

---
+++ conexao.exemplo.php
@@ -0,0 +1,10 @@
+<?php 
+// Renomeie este arquivo para conexao.php e preencha com seus dados reais
+$host = 'localhost';
+$port = '5432';
+$dbname = 'nome_do_banco';
+$user = 'usuario';
+$password = 'senha';
+
+// ... resto do código do PDO
+?>
